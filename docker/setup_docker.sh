6e70c175f90516c7f687f6eb7753e0c6054e2686
Priyanka Kukreja <priyankakukreja267@gmail.com>
2016-03-11 16:59:57 +0530

    Changes for 11th March 2016

--- docker/setup_docker.sh
+++ docker/setup_docker.sh
@@ -8,29 +8,27 @@
 sudo apt-get -y update && sudo apt-get -y install openjdk-7-jdk && export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64 && export PATH=$JAVA_HOME/bin:$PATH
 
 # Setup Maven
-sudo apt-get -y install wget && wget -P /$HOME http://a.mbbsindia.com/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz && tar -xvf /$HOME/apache-maven-3.3.9-bin.tar.gz -C /$HOME/
+sudo apt-get -y install wget && wget -P . http://a.mbbsindia.com/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz && tar -xvf apache-maven-3.3.9-bin.tar.gz -C .
 
 # Install Zeppelin
-sudo apt-get -y install git && git clone https://github.com/apache/incubator-zeppelin.git /$HOME/src/incubator-zeppelin
+sudo apt-get -y install git && git clone https://github.com/apache/incubator-zeppelin.git
 
 # Get Quark Jar
-/$HOME/apache-maven-3.3.9/bin/mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=-DrepoUrl=http://repo1.maven.org/maven2 -Dartifact=com.qubole:quark-jdbc:4.2.0
+apache-maven-3.3.9/bin/mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=-DrepoUrl=http://repo1.maven.org/maven2 -Dartifact=com.qubole:quark-jdbc:4.2.0
 
 # Build Zeppelin
-cd /$HOME/src/incubator-zeppelin
-/$HOME/apache-maven-3.3.9/bin/mvn clean package -DskipTests
+cd incubator-zeppelin
+../apache-maven-3.3.9/bin/mvn clean package -DskipTests
+cd ..
 
 # Copy quark jar -- to current location, where dockerfile is!
-sudo cp /$HOME/.m2/repository/com/qubole/quark-jdbc/4.2.0/quark-jdbc-4.2.0.jar /$HOME/src/incubator-zeppelin/interpreter/jdbc/
-
-#Copy Zeppelin Jars to the folder containing Dockerfile
+sudo cp /$HOME/.m2/repository/com/qubole/quark-jdbc/4.2.0/quark-jdbc-4.2.0.jar ./incubator-zeppelin/interpreter/jdbc/
 
 # Build the image
-#sudo docker build -t $1:$2 .
+sudo docker build -t $1:$2 .
 
 # run with start zeppelin cmd -- take care container does not stop
-#sudo docker run -d --name=$3 -p 0.0.0.0:8080:8080 $1:$2 tail -f /dev/null
+sudo docker run -d --name=$3 -p 0.0.0.0:8080:8080 $1:$2 tail -f /dev/null
 
-#sudo docker exec -it $3 /home/ubuntu/src/incubator-zeppelin/bin/zeppelin-daemon.sh start
-
+sudo docker exec -it $3 $HOME/incubator-zeppelin/bin/zeppelin-daemon.sh start
 
